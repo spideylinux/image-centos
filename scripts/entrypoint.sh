d02e100e350930f7a81b20592f5f68883095c1bf
DroneCI Packer <drone.packer@packet.com>
2019-08-08 18:01:30 -0400

    Update directory check

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 
 #echo "Hello World" > /root/FILE_ON_IMAGE.txt
 
-if [ -d "./tools/packet-packer" ]; then
+if [ -d "./tools/packet-packer/scripts" ]; then
 	echo "Packet's production Packer.io tools..."
 	./tools/packet-packer/scripts/runtime.sh
 fi
